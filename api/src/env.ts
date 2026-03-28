3a40acfc4fbe882cc04a85772adf3e57ab32e149
brenofelips <brendo.filipe2050@outlook.com>
2026-03-28 14:25:07 -0300

    fix: ajsut to server not up

--- api/src/env.ts
+++ api/src/env.ts
@@ -1,12 +1,12 @@
-import { z } from "zod";
+import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z
-    .enum(["development", "production", "test"])
-    .default("development"),
+    .enum(['development', 'production', 'test'])
+    .default('development'),
   PORT: z.coerce.number().default(3333),
   GOOGLE_GENERATIVE_AI_API_KEY: z.string(),
   DATABASE_URL: z.url(),
-});
+})
 
-export const env = envSchema.parse(process.env);
+export const env = envSchema.parse(process.env)
